b31b133a6f23cb27fd2c91747af561d05268fe7e
Wojciech Nawrocki <wjn@lean-fro.org>
2026-08-12 19:46:20 +0000

    fix: vbp versions and metric names

--- bench.py
+++ bench.py
@@ -149,6 +149,13 @@ def checkout_project(
                     # Remark: Verso and mathlib/nightly-testing-* must be on the same toolchain
                     # for mathlib's cache to successfully download.
                     lines[index] = f'require mathlib from git "https://github.com/leanprover-community/mathlib4-nightly-testing.git" @ "{verso_lean_version.replace("nightly", "nightly-testing")}"' 
+                elif re.match(r"^require VersoBlueprint from ", line):
+                    # VersoBlueprint does not publish -rc tags.
+                    project_lean_trunc = re.sub(r'-rc\d+$', '', project_lean_version)
+                    verso_lean_trunc = re.sub(r'-rc\d+$', '', verso_lean_version)
+                    lines[index] = line.replace(
+                        f'"{project_lean_trunc}"', f'"{verso_lean_trunc}"'
+                    )
                 elif re.match(r"^package", line) and useO0:
                     lines[index] = line + '  moreLeancArgs := #["-O0"]\n'
                 else:
